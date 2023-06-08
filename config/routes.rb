446563ecc1ecec1b248a9ab7db35cdfddbfd792a
Hayate_T <hayatesato1@gmail.com>
2023-06-09 00:54:39 +0900

    みんなの写真を追加したよ

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
   # ! サイトのトップページ
   root to: "pages#top_page"
 
-  # ! メンバーのプロフィールページ
-  # * 竹田のプロフィールページ
-  get "takeda", to: "pages#takeda_page"
-  # * 竹中のプロフィールページ
-  get "takenaka", to: "pages#takenaka_page"
-  # * 矢田のプロフィールページ
-  get "yata", to: "pages#yata_page"
-  # * 重山のプロフィールページ
-  get "shigeyama", to: "pages#shigeyama_page"
-  # * 山本のプロフィールページ
-  get "yamamoto", to: "pages#yamamoto_page"
+  # # ! メンバーのプロフィールページ
+  # # * 竹田のプロフィールページ
+  # get "takeda", to: "pages#takeda_page"
+  # # * 竹中のプロフィールページ
+  # get "takenaka", to: "pages#takenaka_page"
+  # # * 矢田のプロフィールページ
+  # get "yata", to: "pages#yata_page"
+  # # * 重山のプロフィールページ
+  # get "shigeyama", to: "pages#shigeyama_page"
+  # # * 山本のプロフィールページ
+  # get "yamamoto", to: "pages#yamamoto_page"
 
   # ! My_closetページ
   # * サービス紹介ページ
